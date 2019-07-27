9d4dc9b41aaeae363058cebeff6fb6dcad0e3b93
ilya-kuznetsov <et.anchoret@gmail.com>
2019-07-27 23:25:31 +0300

    User entity was added.

--- src/main/scala/com/ran/chatterley/Main.scala
+++ src/main/scala/com/ran/chatterley/Main.scala
@@ -1,5 +1,13 @@
 package com.ran.chatterley
 
-object Main extends App {
+import com.ran.chatterley.config.DbConfiguration
+import com.ran.chatterley.entity.User
+import com.typesafe.config.ConfigFactory
+
+object Main extends App with DbConfiguration {
   println("Chatterley started")
+  println(ConfigFactory.load().getString("db.db.url"))
+  println(config.config)
+  val user = User(Some(1), "me", "43224")
+
 }
